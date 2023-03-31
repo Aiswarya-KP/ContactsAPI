c5909e8237c2a57d02499d7ffd175a9e618f7cd6
Aiswarya Kizhakkedath <aiswaryakp2k@gmail.com>
2023-03-31 18:48:25 +0530

    ContactsAPI Created

---
+++ ContactsAPI/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactsAPI.Models
+{
+    public class Contact
+    {
+        public Guid id { get; set; }
+        public String FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+        
+
+
+    }
+}
+
